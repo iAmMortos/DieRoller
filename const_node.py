2b0f9420af73215014dbec86bdbb5d264f42c189
Taylor Lopez <taylor.d.lopez@lmco.com>
2019-12-05 17:40:54 -0700

    Removing unnecessary files

--- const_node.py
+++
@@ -1,8 +0,0 @@
-from die_func_node import DieFuncNode
-from die_func_node_type import DieFuncNodeType
-
-
-class ConstNode(DieFuncNode):
-  def __init__(self):
-    super().__init__()
-    self.type = DieFuncNodeType.const
